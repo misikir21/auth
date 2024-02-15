6ebb450781d1690d0b27d7cd04ac9bfcd5b834e3
misikir21 <misikirkaka@gmail.com>
2024-02-15 15:51:31 +0300

    complete the connection

--- src/dbconfig/dbconfig.ts
+++ src/dbconfig/dbconfig.ts
@@ -1,7 +1,17 @@
 import mongoose from "mongoose";
 export async function connect() {
   try {
-    mongoose.connect(process.env.MONGO_URL);
+    mongoose.connect(process.env.MONGO_URL!);
+    const connection = mongoose.connection;
+    connection.on("connected", () => {
+      console.log("Mongodb connected successfully");
+    });
+    connection.on("error", (err) => {
+      console.log(
+        "mongodb connection error.please make sure mongodb is running" + err
+      );
+      process.exit();
+    });
   } catch (error) {
     console.log("something went wrong!");
     console.log(error);
