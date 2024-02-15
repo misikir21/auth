480287cea23f0cae9e8520acf1402423c8c16498
misikir21 <misikirkaka@gmail.com>
2024-02-15 16:12:17 +0300

    add fetch to my page

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+import React from "react";
+import { useRouter } from "next/navigation";
+import { Axios } from "axios";
+function signup() {
+  const [user, setuser] = React.useState({
+    email: "",
+    password: "",
+    username: "",
+  });
+
+  return (
+    <div className="flex">
+      <h1 className="text-center text-white">sign up</h1>;
+    </div>
+  );
+}
+
+export default signup;
