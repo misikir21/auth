ee2d27b1cb1d7ec7580ea8d003036fc52c2464ee
misikir21 <misikirkaka@gmail.com>
2024-02-15 17:11:31 +0300

    add a login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,47 @@
+"use client";
+import React from "react";
+import { useRouter } from "next/navigation";
+import { Axios } from "axios";
+import Link from "next/link";
+function login() {
+  const [user, setuser] = React.useState({
+    email: "",
+    password: "",
+  });
+  const onlogin = async () => {};
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1> login</h1>
+      <hr />
+      <label htmlFor="email">email</label>
+      <input
+        className="p-2 border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        type="text"
+        id="email"
+        value={user.email}
+        onChange={(e) => setuser({ ...user, email: e.target.value })}
+        placeholder="email"
+      />
+
+      <label htmlFor="password">password</label>
+      <input
+        className="p-2 border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        type="password"
+        id="password"
+        value={user.password}
+        onChange={(e) => setuser({ ...user, password: e.target.value })}
+        placeholder="password"
+      />
+      <button
+        onClick={onlogin}
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+      >
+        login here
+      </button>
+      <Link href="/login">Visit signup page</Link>
+    </div>
+  );
+}
+
+export default login;
