c3c57e9269c8d6e5c18a8b485dee9d3323090b64
misikir21 <misikirkaka@gmail.com>
2024-02-16 18:40:55 +0300

    check if a user email already exist

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -1,6 +1,20 @@
 import { connect } from "@/dbconfig/dbconfig";
-import User from "@/models/usermodels";
+import User from "@models/usermodels.js";
 import { NextRequest, NextResponse } from "next/server";
-import bycyptjs from "byryptsjs";
+import bcryptjs from "bcryptjs";
 
 connect();
+
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+    const { usename, email, password } = reqBody;
+    console.log(reqBody);
+    const user = await User.findOne({ email });
+    if (user) {
+      return NextResponse.json({ error: "user exist" }, { status: 400 });
+    }
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 5000 });
+  }
+}
